654b1291e4d0c752287828ae11410094d03190d9
Yuta Inoue <kfyvic@gmail.cim>
2016-11-23 17:48:34 +0900

    Make create method in groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,11 @@
 class GroupsController < ApplicationController
+  def create
+    Group.create(create_params)
+    redirect_to controller: :messages, action: :index
+  end
+
+  private
+  def create_params
+    params.require(:group).permit(:name)
+  end
 end
