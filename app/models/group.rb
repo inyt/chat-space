5f7d9d7ed8fd46b8d195a38adf3f9715cee4943e
Yuta Inoue <kfyvic@gmail.cim>
2016-11-25 11:56:04 +0900

    Edit groups.rb
    accepts_nested_attributes_for :users

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ApplicationRecord
+  validates :name, presence: true
   has_many :groups_users
   has_many :users, through: :groups_users
-  validates :name, presence: true
+  accepts_nested_attributes_for :users
 end
