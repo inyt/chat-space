57620323972b201060db7e84549f85d9f8694eba
Yuta Inoue <kfyvic@gmail.cim>
2016-11-26 13:05:17 +0900

    revise messages controller to create messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,16 @@
 class MessagesController < ApplicationController
+
   def index
     @message = Message.new
+  end
+
+  def create
+    Message.create(message_params)
+    redirect_to root_url, notice: "メッセージが投稿されました。"
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body)
   end
 end
