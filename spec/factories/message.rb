c0ae7f49845b8315a26a4174566ada7bc8c37e78
Yuta Inoue <kfyvic@gmail.cim>
2016-11-29 18:29:40 +0900

    use factorygirl

---
+++ spec/factories/message.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :message do
+    body       "hello"
+    image      ""
+    user_id    1
+    group_id   1
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 describe Message do
   describe '#create' do
     it "is valid with a body" do
-      message = Message.new(body: "a", image: "", user_id: 1, group_id: 1)
+      message = build(:message)
       expect(message).to be_valid
     end
 
     it "is invalid without a body" do
-      message = Message.new(body: "", image: "", user_id: 1, group_id: 1)
+      message = build(:message, body: "")
       message.valid?
       expect(message.errors[:body]).to include("を入力してください")
     end
