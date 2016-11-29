d1323f2b636d6762834e771130811101b75b64c1
Yuta Inoue <kfyvic@gmail.cim>
2016-11-29 12:33:41 +0900

    modify route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :groups, only: [:edit, :new, :create, :update, :show] do
+  resources :groups, only: [:edit, :new, :create, :update, :show, :index] do
     resources :messages, only: [:index, :create]
   end
+  root 'groups#index'
 end
