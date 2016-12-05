aac6adf69ef8428a495262208bcc2e4bfdce4971
Yuta Inoue <kfyvic@gmail.cim>
2016-12-05 14:09:58 +0900

    make users#search

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'users/search' => 'users#search'
   resources :groups, only: [:edit, :new, :create, :update, :show, :index] do
     resources :messages, only: [:index, :create]
   end
