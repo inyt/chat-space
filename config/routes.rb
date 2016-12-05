f347e8abfffa4d8787e8cd5ff810f382e07269ae
Yuta Inoue <kfyvic@gmail.cim>
2016-12-05 16:16:16 +0900

    refactoring of routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'users/search' => 'users#search'
-  resources :groups, only: [:edit, :new, :create, :update, :show, :index] do
+  resources :groups, except: :destroy do
     resources :messages, only: [:index, :create]
   end
   root 'groups#index'
